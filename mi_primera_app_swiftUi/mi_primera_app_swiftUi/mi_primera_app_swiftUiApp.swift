1499382a63ed3745ba8eb5d0d6ce832ad5149ed0
Astrid C. Sanchez L <astridsanchez@WMDMPF46Y0HG.wks.jnj.com>
2025-02-21 10:05:05 -0700

    [Agregar] mi primera app usando swiftUi

---
+++ mi_primera_app_swiftUi/mi_primera_app_swiftUi/mi_primera_app_swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mi_primera_app_swiftUiApp.swift
+//  mi_primera_app_swiftUi
+//
+//  Created by Astrid C. Sanchez L. on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct mi_primera_app_swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
